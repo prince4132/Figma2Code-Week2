8f60e168c103faea92620a3440608215b3a3a7a6
Prince <princeekpinse97@gmail.com>
2024-08-07 10:15:29 +0100

    modif header

--- app/layout.js
+++ app/layout.js
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
       </head>
-      <body className='font-archivo-regular'>
+      <body className='font-archivo-regular text-noir'>
         <Header />
         {children}
       </body>
